a7ee12d961a1c6ac1d74ee68c112cdba27541eb3
Rubyat Jesmin Shammi <86279535+Rubyat-Jesmin-Shammi@users.noreply.github.com>
2021-09-22 14:39:15 +0600

    Add files via upload

---
+++ Final/Project/bappo.php
@@ -0,0 +1,289 @@
+<?php
+session_start();
+//error_reporting(0);
+include('include/config.php');
+include('include/checklogin.php');
+check_login();
+
+if(isset($_POST['submit']))
+{
+$specilization=$_POST['Doctorspecialization'];
+$doctorid=$_POST['doctor'];
+$userid=$_SESSION['id'];
+$fees=$_POST['fees'];
+$appdate=$_POST['appdate'];
+$time=$_POST['apptime'];
+$userstatus=1;
+$docstatus=1;
+$query=mysqli_query($con,"insert into appointment(doctorSpecialization,doctorId,userId,consultancyFees,appointmentDate,appointmentTime,userStatus,doctorStatus) values('$specilization','$doctorid','$userid','$fees','$appdate','$time','$userstatus','$docstatus')");
+	if($query)
+	{
+		echo "<script>alert('Your appointment successfully booked');</script>";
+	}
+
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Patient  | Book Appointment</title>
+	  <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <!-- Bootstrap CSS -->
+    
+        <link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
+
+    
+		<script>
+function getdoctor(val) {
+	$.ajax({
+	type: "POST",
+	url: "get_doctor.php",
+	data:'specilizationid='+val,
+	success: function(data){
+		$("#doctor").html(data);
+	}
+	});
+}
+</script>	
+
+
+<script>
+function getfee(val) {
+	$.ajax({
+	type: "POST",
+	url: "get_doctor.php",
+	data:'doctor='+val,
+	success: function(data){
+		$("#fees").html(data);
+	}
+	});
+}
+</script>	
+
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans&display=swap" rel="stylesheet">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+  <a class="navbar-brand" href="#"><i class="fa fa-user-plus" aria-hidden="true"></i> Hospital Management System </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <style >
+    .bg-primary {
+    background: -webkit-linear-gradient(left, #3931af, #00c6ff);
+}
+.list-group-item.active {
+    z-index: 2;
+    color: #fff;
+    background-color: #342ac1;
+    border-color: #007bff;
+}
+.text-primary {
+    color: #342ac1!important;
+}
+
+.btn-primary{
+  background-color: #3c50c1;
+  border-color: #3c50c1;
+}
+  </style>
+
+ <div class="collapse navbar-collapse" id="navbarSupportedContent">
+     <ul class="navbar-nav mr-auto">
+       <li class="nav-item">
+        <a class="nav-link" href="logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i>Logout</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#"></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+	</head>
+	 <style type="text/css">
+    button:hover{cursor:pointer;}
+    #inputbtn:hover{cursor:pointer;}
+  </style>
+	<body style="padding-top:50px;">
+		<div class="container-fluid" style="margin-top:50px;">
+    <h3 style = "margin-left: 40%;  padding-bottom: 20px; font-family: 'IBM Plex Sans', sans-serif;"> Welcome to Hospital Management System
+   </h3>
+    <div class="row">
+  <div class="col-md-4" style="max-width:25%; margin-top: 3%">
+    <div class="list-group" id="list-tab" role="tablist">
+      <a class="list-group-item list-group-item-action"href="panel.php">Dashboard</a>
+      <a class="list-group-item list-group-item-action active" href="bappo.php">Book Appointment</a>
+      <a class="list-group-item list-group-item-action" href="appo.php">Appointment History</a>
+      <a class="list-group-item list-group-item-action" href="edit.php">Change Password</a>
+      
+    </div><br>
+  </div>
+<div class="col-md-8" style="margin-top: 3%;">
+						<div class="container-fluid container-fullw bg-white">
+							<div class="row">
+								<div class="col-md-12">
+									
+									<div class="row margin-top-30">
+										<div class="col-lg-8 col-md-12">
+											<div class="panel panel-white">
+												<div class="panel-heading">
+													<h5 class="panel-title">Book Appointment</h5>
+												</div>
+												<div class="panel-body">
+									
+													<form role="form" name="book" method="post" >
+														
+
+
+<div class="form-group">
+															<label for="DoctorSpecialization">
+																Doctor Specialization
+															</label>
+							<select name="Doctorspecialization" class="form-control" onChange="getdoctor(this.value);" required="required">
+																<option value="">Select Specialization</option>
+<?php $ret=mysqli_query($con,"select * from doctorspecilization");
+while($row=mysqli_fetch_array($ret))
+{
+?>
+																<option value="<?php echo htmlentities($row['specilization']);?>">
+																	<?php echo htmlentities($row['specilization']);?>
+																</option>
+																<?php } ?>
+																
+															</select>
+														</div>
+
+
+
+
+														<div class="form-group">
+															<label for="doctor">
+																Doctors
+															</label>
+						<select name="doctor" class="form-control" id="doctor" onChange="getfee(this.value);" required="required">
+						<option value="">Select Doctor</option>
+						</select>
+														</div>
+
+
+
+
+
+														<div class="form-group">
+															<label for="consultancyfees">
+																Consultancy Fees
+															</label>
+					<select name="fees" class="form-control" id="fees"  readonly>
+						
+						</select>
+														</div>
+														
+<div class="form-group">
+															<label for="AppointmentDate">
+																Date
+															</label>
+<input class="form-control datepicker" name="appdate"  required="required" data-date-format="yyyy-mm-dd">
+	
+														</div>
+														
+<div class="form-group">
+							<label for="Appointmenttime">
+														
+								Time
+													
+							</label>
+			
+			<select name="apptime" class="form-control" id="apptime" required="required">
+                      <option value="" disabled selected>Select Time</option>
+                      <option value="08:00:00">8:00 AM</option>
+                      <option value="10:00:00">10:00 AM</option>
+                      <option value="12:00:00">12:00 PM</option>
+                      <option value="14:00:00">2:00 PM</option>
+                      <option value="16:00:00">4:00 PM</option>
+                    </select>
+			
+			
+</div>														
+														
+														<button type="submit" name="submit" class="btn btn-o btn-primary">
+															Submit
+														</button>
+													</form>
+												</div>
+											</div>
+										</div>
+											
+											</div>
+										</div>
+									
+									</div>
+								</div>
+							
+						<!-- end: BASIC EXAMPLE -->
+			
+					
+					</div>
+						
+						
+					
+						<!-- end: SELECT BOXES -->
+						
+					</div>
+			
+		
+		 </div>
+		 </div>
+		<!-- start: MAIN JAVASCRIPTS -->
+		<script src="vendor/jquery/jquery.min.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+		<script src="vendor/modernizr/modernizr.js"></script>
+		<script src="vendor/jquery-cookie/jquery.cookie.js"></script>
+		<script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+		<script src="vendor/switchery/switchery.min.js"></script>
+		<!-- end: MAIN JAVASCRIPTS -->
+		<!-- start: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+		<script src="vendor/maskedinput/jquery.maskedinput.min.js"></script>
+		<script src="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
+		<script src="vendor/autosize/autosize.min.js"></script>
+		<script src="vendor/selectFx/classie.js"></script>
+		<script src="vendor/selectFx/selectFx.js"></script>
+		<script src="vendor/select2/select2.min.js"></script>
+		<script src="vendor/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+		<script src="vendor/bootstrap-timepicker/bootstrap-timepicker.min.js"></script>
+		<!-- end: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+		<!-- start: CLIP-TWO JAVASCRIPTS -->
+		<script src="assets/js/main.js"></script>
+		<!-- start: JavaScript Event Handlers for this page -->
+		<script src="assets/js/form-elements.js"></script>
+		<script>
+			jQuery(document).ready(function() {
+				Main.init();
+				FormElements.init();
+			});
+
+			$('.datepicker').datepicker({
+    format: 'yyyy-mm-dd',
+    startDate: '-3d'
+});
+		</script>
+		  <script type="text/javascript">
+            $('#timepicker1').timepicker();
+        </script>
+		<!-- end: JavaScript Event Handlers for this page -->
+		<!-- end: CLIP-TWO JAVASCRIPTS -->
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+
+	</body>
+</html>
